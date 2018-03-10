635db5d1ee7cf05d971da89876e312415f2379b6
Brandon Chin <brandonchin@me.com>
2018-03-10 23:41:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   end #def self.new_from_filename
 
   def self.create_from_filename(filename)
-    
+    self.new_from_filename(filename)
   end #def self.create_from_filename
 
 end #class Song
